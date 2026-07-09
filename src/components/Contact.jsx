d22afc719d655ebe0e59d03ec05ba8f302e49961
Mayurraj Singh Rathore <mayurraj.singh@webcluesinfotech.com>
2026-07-09 16:15:02 +0530

    add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,39 @@
-import profileImage from '../assets/profile.jpg';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin, FaInstagram, FaTwitter } from 'react-icons/fa';
+
 const Contact = () => {
   return (
     <section id="contact" className="py-16">
-      <h2 className="mb-4 text-2xl font-semibold text-white">Contact</h2>
-     <img src={profileImage} alt="profile" className="mb-4" />
+      <h2 className="mb-4 text-2xl font-bold text-white">Contact</h2>
+    
       <p className="text-neutral-300">You can reach me through email or LinkedIn.</p>
+      
+      <ul className="social-icons">
+  <li>
+    <a href="https://github.com" target="_blank" rel="noopener noreferrer" mb-4>
+      <FaGithub />
+    </a>
+  </li>
+
+  <li>
+    <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+      <FaLinkedin />
+    </a>
+  </li>
+
+  <li>
+    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+      <FaInstagram />
+    </a>
+  </li>
+
+  <li>
+    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+      <FaTwitter />
+    </a>
+  </li>
+</ul>
+
     </section>
   );
 };
