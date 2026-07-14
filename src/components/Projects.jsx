e4fa17a7a25bfadf7efdf78d11b3cbff5eff575e
Mayurraj Singh Rathore <mayurraj.singh@webcluesinfotech.com>
2026-07-14 17:36:18 +0530

    add

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -192,6 +192,8 @@ const Projects = () => {
                 <span className="rounded-md bg-neutral-800 px-3 py-1 text-sm text-purple-300">React</span>
                 <span className="rounded-md bg-neutral-800 px-3 py-1 text-sm text-neutral-200">Tailwind</span>
                 <span className="rounded-md bg-neutral-800 px-3 py-1 text-sm text-violet-300">Responsive</span>
+                 <span className="rounded-md bg-neutral-800 px-3 py-1 text-sm text-violet-300">html,js,css</span>
+                
               </div>
 
               <div className="mt-6 flex flex-wrap gap-3">
@@ -229,7 +231,6 @@ const Projects = () => {
               <div className="mt-4 flex flex-wrap gap-2">
                 <span className="rounded-md bg-neutral-800 px-3 py-1 text-sm text-purple-300">React</span>
                 <span className="rounded-md bg-neutral-800 px-3 py-1 text-sm text-neutral-200">Tailwind</span>
-                <span className="rounded-md bg-neutral-800 px-3 py-1 text-sm text-neutral-200">UI/UX</span>
                 <span className="rounded-md bg-neutral-800 px-3 py-1 text-sm text-violet-300">Responsive</span>
               </div>
 
